2870a173ddf6650b46d074157391d0d1375265ec
Noé Muñoz Sánchez <noesm.16@gmail.com>
2021-06-07 17:06:45 -0500

    Finished UI, added Masonry and finished project

---
+++ src/components/layout.js
@@ -0,0 +1,122 @@
+import React from 'react';
+
+import { useHistory, useLocation } from 'react-router';
+
+import { Drawer, makeStyles, Typography } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import Avatar from '@material-ui/core/Avatar';
+
+import { AddCircleOutlined, SubjectOutlined } from '@material-ui/icons';
+
+import { format } from 'date-fns';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: '#f9f9f9',
+      width: '100%',
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    root: {
+      display: 'flex',
+    },
+    active: {
+      background: '#f4f4f4',
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    appbar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+    toolbar: theme.mixins.toolbar,
+    date: {
+      flexGrow: 1,
+    },
+    avatar: {
+      marginLeft: theme.spacing(2),
+    },
+  };
+});
+
+export default function Layout({ children }) {
+  const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+
+  const menuItems = [
+    {
+      text: 'My Notes',
+      icon: <SubjectOutlined color='secondary' />,
+      path: '/',
+    },
+    {
+      text: 'Create Note',
+      icon: <AddCircleOutlined color='secondary' />,
+      path: '/create',
+    },
+  ];
+
+  return (
+    <div className={classes.root}>
+      {/* App bar */}
+      <AppBar className={classes.appbar} elevation={0}>
+        <ToolBar>
+          <Typography className={classes.date}>
+            Today is the {format(new Date(), 'do MMMM Y')}
+          </Typography>
+          <Typography>Mario</Typography>
+          <Avatar src='/mario-av.png' className={classes.avatar} />
+        </ToolBar>
+      </AppBar>
+      {/* Side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant='permanent'
+        anchor='left'
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant='h5' className={classes.title}>
+            Ninja Notes
+          </Typography>
+        </div>
+        {/* List/ Links */}
+        <List>
+          {menuItems.map((item) => (
+            <ListItem
+              key={item.text}
+              button
+              onClick={() => history.push(item.path)}
+              className={
+                location.pathname === item.path ? classes.active : null
+              }
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        {children}
+      </div>
+    </div>
+  );
+}
